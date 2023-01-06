f6002b379f60db06aed2f4324d7254ddbeb4078e
Maimu Kohagura <e225711@ie.u-ryukyu.ac.jp>
2023-01-06 21:31:37 +0900

    done for Judge.java

--- app/src/main/java/jp/ac/uryukyu/ie/e225711/Judge.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225711/Judge.java
@@ -1,5 +1,11 @@
 package jp.ac.uryukyu.ie.e225711;
 
 public class Judge {
-  
+  public boolean winJudge(Player name) {
+    if (name.remainFinger.size() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e225711/Player.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225711/Player.java
@@ -1,14 +1,15 @@
 package jp.ac.uryukyu.ie.e225711;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Player {
   private String name;
-  List<Integer> remainFinger = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+  List<Integer> remainFinger = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
   List<Integer> usedFinger = new ArrayList<Integer>();
 
-  public String setName(String name){
+  public String setName(String name) {
     this.name = name;
     return this.name;
   }
