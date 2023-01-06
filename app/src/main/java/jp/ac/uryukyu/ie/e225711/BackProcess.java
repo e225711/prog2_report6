3022e14894e3d28eb56fbab5e8946871b0894c2d
Maimu Kohagura <e225711@ie.u-ryukyu.ac.jp>
2023-01-07 02:04:10 +0900

    done for BackProcess.java

--- app/src/main/java/jp/ac/uryukyu/ie/e225711/BackProcess.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225711/BackProcess.java
@@ -1,5 +1,48 @@
 package jp.ac.uryukyu.ie.e225711;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BackProcess {
-  
+  Player player = new Player();
+
+  private boolean canUseFinger;
+  private String fingerNum;
+
+  public boolean getCanUseFinger() {
+    return canUseFinger;
+  }
+
+  public void setCanUseFinger(boolean canUseFinger) {
+    this.canUseFinger = canUseFinger;
+  }
+
+  public String getFingerNum1() {
+    return fingerNum;
+  }
+
+  public void setFingerNum1(String fingerNum) {
+    this.fingerNum = fingerNum;
+  }
+
+  public void arrangementFinger(int num) {
+    player.remainFinger.remove(player.remainFinger.indexOf(num));
+    player.usedFinger.add(num);
+  }
+
+  public void getInput() {
+    System.out.println("使用する数字を入力してください");
+
+    Scanner scan = new java.util.Scanner(System.in);
+    String fingerNum = scan.next();
+
+    if (fingerNum.equals("パス")) {
+      setCanUseFinger(false);
+    } else {
+      int intFingerNum = Integer.parseInt(fingerNum);
+      arrangementFinger(intFingerNum);
+    }
+
+  }
 }
